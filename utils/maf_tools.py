0637a8d5b3c49539a6cbe195fadaa52bb06f6d7c
Masoud <mpcl5mail@gmail.com>
2024-10-20 07:16:38 +0000

    added maf tools

---
+++ utils/maf_tools.py
@@ -0,0 +1,47 @@
+import torch
+
+
+class MAFHelper():
+    mafs: torch.Tensor
+    maf_categories: torch.Tensor
+
+    def __init__(self, mafs, num_groups) -> None:
+        self.mafs = mafs
+        self.num_groups = num_groups
+        self.group_boundaries = self._get_bouderies()
+
+    def get_data(self):
+        return (
+            self.mafs.clone(),
+            self.maf_categories.clone()
+        )
+
+    def _get_bouderies(self):
+        sorted_numbers = torch.sort(self.mafs).values
+
+        n = len(sorted_numbers)
+        split_indices = [int(n * i / self.num_groups)
+                         for i in range(1, self.num_groups)]
+        group_boundaries = [sorted_numbers[0].item()] + [sorted_numbers[idx].item()
+                                                         for idx in split_indices] + [sorted_numbers[-1].item()]
+        group_boundaries = [round(bound, 3) if bound >
+                            1e-6 else 0.0 for bound in group_boundaries]
+
+        return group_boundaries
+
+    def _get_categories(self, maf):
+        for i in range(1, len(self.group_boundaries)):
+            if self.group_boundaries[i] <= maf < self.group_boundaries[i + 1]:
+                return i
+
+        return len(self.group_boundaries) - 1
+
+    def _get_categorized_mafs(self):
+        maf_categories = torch.zeros_like(
+            self.mafs, device=self.mafs.device, dtype=torch.int16)
+
+        for idx, maf_value in enumerate(self.mafs):
+            maf_categories[idx] = self._get_categories(
+                maf_value, self.group_boundaries)
+
+        return maf_categories
